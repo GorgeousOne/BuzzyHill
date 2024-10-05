ed80aff311f6dee66da08a7fca26285767682d24
IEatSushi <2klaus.4peter@gmail.com>
2024-10-05 12:28:14 +0200

    fix fixedupdate abuse

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -24,6 +24,8 @@ public class PlayerMovement : MonoBehaviour {
 
 	private void OnEnable() {
 		controls = new PlayerControls();
+		controls.Player.Jump.performed += _ => jumpPressed = true;
+		controls.Player.Jump.performed += _ => jumpPressed = true;
 		controls.Enable();
 	}
 
@@ -35,7 +37,15 @@ public class PlayerMovement : MonoBehaviour {
 		rigid = GetComponent<Rigidbody2D>();
 	}
 
+	private bool jumpPressed;
+	private bool jumpReleased;
+	private float horizontalInput;
+	
 	void Update() {
+		horizontalInput = controls.Player.Move.ReadValue<float>();
+	}
+
+	private void FixedUpdate() {
 		Vector2 v2GroundedBoxCheckPosition = (Vector2) transform.position + new Vector2(0, -0.01f);
 		Vector2 v2GroundedBoxCheckScale = (Vector2) transform.localScale + new Vector2(-0.02f, 0);
 		bool bGrounded = Physics2D.OverlapCapsule(v2GroundedBoxCheckPosition, v2GroundedBoxCheckScale, 0, lmWalls);
@@ -46,11 +56,11 @@ public class PlayerMovement : MonoBehaviour {
 		}
 
 		fJumpPressedRemember -= Time.deltaTime;
-		if (controls.Player.Jump.WasPressedThisFrame()) {
+		if (jumpPressed) {
 			fJumpPressedRemember = fJumpPressedRememberTime;
 		}
 
-		if (controls.Player.Jump.WasReleasedThisFrame()) {
+		if (jumpReleased) {
 			if (rigid.velocity.y > 0) {
 				rigid.velocity = new Vector2(rigid.velocity.x, rigid.velocity.y * fCutJumpHeight);
 			}
@@ -63,7 +73,6 @@ public class PlayerMovement : MonoBehaviour {
 		}
 
 		float fHorizontalVelocity = rigid.velocity.x;
-		float horizontalInput = controls.Player.Move.ReadValue<float>();
 		fHorizontalVelocity += horizontalInput * fHorizontalAcceleration;
 
 		if (Mathf.Abs(horizontalInput) < 0.01f)
@@ -74,5 +83,7 @@ public class PlayerMovement : MonoBehaviour {
 			fHorizontalVelocity *= Mathf.Pow(1f - fHorizontalDampingBasic, Time.deltaTime * 10f);
 
 		rigid.velocity = new Vector2(fHorizontalVelocity, rigid.velocity.y);
+		jumpPressed = false;
+		jumpReleased = false;
 	}
 }
